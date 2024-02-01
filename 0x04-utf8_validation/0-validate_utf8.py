90f5395c449b45f21de77e9b509f304212deacdd
Guy Ahonakpon GBAGUIDI <guygbaguidi123root@gmail.com>
2024-02-01 17:14:21 +0100

    fix: check error

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -18,13 +18,16 @@ def validUTF8(data):
     count = 0
     for byte in data:
         if(count == 0):
+            if ((byte >> 7) == 0):
+                continue
+
             if ((byte >> 5) == 0b110):
                 count = 1
             elif ((byte >> 4) == 0b1110):
                 count = 2
             elif ((byte >> 3) == 0b11110):
                 count = 3
-            elif ((byte >> 7) != 0):
+            else:
                 return False
         else:
             if ((byte >> 6) != 0b10):
